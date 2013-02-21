83c5aa1bdb71343b07ece0736761a330c2219583
Vadim Asadchi <vadim.asadchiy@gmail.com>
2013-02-21 17:58:29 +0300

    production dashboard

--- js/views/common.js
+++ js/views/common.js
@@ -9,15 +9,38 @@ define([
     var Menu = Backbone.View.extend({
             template:       Handlebars.compile(MenuTemplate),
             el:             $("#menu"),
+            out:            false,
             initialize:     function(opts){
                 this.render();
             },
             render:         function(){
                 var that = this;
                 that.$el.html(that.template());
+                this.$el.find("#buttonNav").on("click",function(){
+                    if(that.out){
+                        that.slideIn();
+                    } else{
+                        that.slideOut();
+                    }
+                    that.out = !that.out;
+                });
                 return this;
+            },
+            slideOut: function(){
+                this.$el.find(".menubox").animate({left: "0px"}, 250);
+                this.$el.find("#buttonNav").animate({left: "125px"}, 250);
+            },
+            slideIn: function(){
+                var that = this;
+                this.$el.find(".menubox").animate({left: "-125px"}, 250,null,function(){
+                    that.$el.find(".menubox").css("left", "");
+                });
+                this.$el.find("#buttonNav").animate({left: "0px"}, 250,null,function(){
+                    that.$el.find("#buttonNav").css("left", "");
+                });
+
             }
-        });
+        }),
         Header = Backbone.View.extend({
             template:       Handlebars.compile(HeaderTemplate),
             el:             $("#header"),
